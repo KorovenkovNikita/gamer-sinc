3a8112494825f33dff737eb50f546b1ea6e67b50
KorovenkovNikita <korovenkov.nik@mail.ru>
2023-09-02 18:31:23 +0300

    first commit

---
+++ src/test/java/com/gsg/gamersync/GamerSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsg.gamersync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
